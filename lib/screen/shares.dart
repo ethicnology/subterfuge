4413b9272a6d94d38507c514d339084305938eb6
ethicnology <ethicnology@pm.me>
2022-06-20 10:47:18 +0200

    feat: SharesScreen display groups and shares

---
+++ lib/screen/shares.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:slip39/slip39.dart';
+import 'package:subterfuge/widget/group.dart';
+
+class SharesScreen extends StatelessWidget {
+  Slip39 slip;
+  SharesScreen({super.key, required this.slip});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Shares')),
+        body: ListView(
+            shrinkWrap: true,
+            children: List.generate(slip.groupCount, (group) {
+              return GroupWidget(
+                  number: '${group + 1}',
+                  shares: slip.fromPath('r/$group').mnemonics);
+            })));
+  }
+}
