ff850a325ef3e93d5b19bb280c2eba642ccd7243
ethicnology <ethicnology@pm.me>
2022-06-20 10:44:12 +0200

    feat: ShareWidget

---
+++ lib/widget/share.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ShareWidget extends StatelessWidget {
+  String number;
+  String share;
+  ShareWidget({super.key, required this.number, required this.share});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        color: Colors.deepPurple,
+        child: Column(
+          children: [
+            Text('Share $number: '),
+            SelectableText(share,
+                showCursor: true,
+                toolbarOptions: const ToolbarOptions(
+                  copy: true,
+                  selectAll: true,
+                  cut: false,
+                  paste: false,
+                ))
+          ],
+        ));
+  }
+}
