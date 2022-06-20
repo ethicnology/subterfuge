49f228dc8b286556526d9850f10f064c7ca1dbba
ethicnology <ethicnology@pm.me>
2022-06-20 10:36:57 +0200

    feat: simple HomeScreen with useless ElevatedButton

---
+++ lib/screen/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Subterfuge')),
+        body: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomeScreen()),
+                );
+              },
+              child: const Text('Create Shared Secret'),
+            ),
+          ],
+        ));
+  }
+}
