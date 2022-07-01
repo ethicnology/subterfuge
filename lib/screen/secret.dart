6bf446b0b3a0d5c2d1ebae655fe6fcec23466a87
ethicnology <ethicnology@pm.me>
2022-07-01 09:42:45 +0200

    feat: basic secret screen

---
+++ lib/screen/secret.dart
@@ -0,0 +1,19 @@
+import 'package:convert/convert.dart';
+import 'package:flutter/material.dart';
+
+class SecretScreen extends StatelessWidget {
+  List<int> secret;
+  SecretScreen({super.key, required this.secret});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Secret')),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text('secret (hex): ${hex.encode(secret)}'),
+          ],
+        ));
+  }
+}
