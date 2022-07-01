3d48aa2e92bcb839efed8e210a164aa8ec2f1b47
ethicnology <ethicnology@pm.me>
2022-07-01 09:43:52 +0200

    feat: implement RecoverSecretScreen to home routing

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:subterfuge/screen/create_secret.dart';
+import 'package:subterfuge/screen/recover_secret.dart';
 import 'package:subterfuge/widget/disclaimer.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -13,6 +14,10 @@ class HomeScreen extends StatelessWidget {
           children: [
             const DisclaimerWidget(),
             ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+                onPrimary: Colors.teal,
+              ),
               onPressed: () {
                 Navigator.push(
                   context,
@@ -21,6 +26,20 @@ class HomeScreen extends StatelessWidget {
                 );
               },
               child: const Text('Create Shared Secret'),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+                onPrimary: Colors.teal,
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const RecoverSecretScreen()),
+                );
+              },
+              child: const Text('Recover Shared Secret'),
             ),
           ],
         ));
