8f19dd0c6e3cf63dde035a8f66ad00af0d6538cb
ethicnology <ethicnology@pm.me>
2022-09-06 10:04:36 +0200

    feat: DisclaimerScreen hardcoded text

--- lib/screen/disclaimer.dart
+++ lib/screen/disclaimer.dart
@@ -6,7 +6,34 @@ class DisclaimerScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Subterfuge')),
-        body: const Text('disclaimer about dependencies'));
+      appBar: AppBar(title: const Text('Disclaimer')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          Text(
+            'This application relies on few dependencies. They are listed in the pubspec.yaml under the dependencies section.',
+          ),
+          Text(
+            '\n- flutter maintained by google, the framework used to create this cross-platform app',
+          ),
+          Text(
+            '\n- slip39-dart maintained by ilap, which is one of the listed reference implementations in the proposal',
+          ),
+          Text(
+            '\n- convert maintained by dart.dev provide encoder/decoder functions',
+          ),
+          SelectableText(
+            '\nsubterfuge: https://github.com/ethicnology/subterfuge',
+          ),
+          SelectableText(
+            '\nslip39: https://github.com/satoshilabs/slips/blob/master/slip-0039.md',
+          ),
+          SelectableText(
+            '\nslip39-dart: https://github.com/ilap/slip39-dart',
+          )
+        ],
+      ),
+    );
   }
 }
