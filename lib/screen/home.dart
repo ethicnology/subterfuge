f736dfcbb316d8597b23e3fdc4c4ef3cf610263b
ethicnology <ethicnology@pm.me>
2022-09-09 15:20:16 +0200

    refactor: SingleChildScrollView, SizedBoxes and Text about Shamir

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -11,7 +11,8 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
             title: const Text('Subterfuge, a secret sharing experience')),
-        body: Column(
+        body: SingleChildScrollView(
+            child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
             const DisclaimerWidget(),
@@ -59,7 +60,18 @@ class HomeScreen extends StatelessWidget {
               },
               child: const Text('Recover a secret'),
             ),
+            const SizedBox(height: 25),
+            const SizedBox(
+              width: 525,
+              child: SelectableText(
+                'Adi Shamir published How to Share a Secret in november 1979.\nDecades later, with the boom of cryptocurrencies, Shamir secret sharing resurface.\nHow to deal with private keys belonging to a group instead of individual such as non-profit organizations and companies…\nIn his scientific publication Shamir presents a threshold schemes ideally suited to applications in which a group of mutually suspicious individuals with conflicting interests must cooperate. Ideally we would like the cooperation to be based on mutual consent.\nSatoshi Lab Improvement Proposal n°39 or SLIP39 is the document that formalize Shamir’s Secret Sharing for Mnemonic Codes, a group of easy to remember words which are widespread to backup secret keys.\nThis application is an attempt to implement SLIP39 in Flutter, an open source framework by Google for building natively compiled, multi-platform (Linux, Web, Android, Windows, MacOs, iOs) applications from a single codebase.',
+                textAlign: TextAlign.justify,
+              ),
+            ),
+            const SizedBox(height: 50),
+            const SelectableText('github.com/ethicnology/subterfuge',
+                style: TextStyle(fontStyle: FontStyle.italic)),
           ],
-        ));
+        )));
   }
 }
