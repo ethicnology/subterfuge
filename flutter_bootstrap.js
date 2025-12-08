3d28fc2edc2e864e685c4b3195078a50b436372e
ethicnology <ethicnology@pm.me>
2025-12-07 20:03:02 -0500

    deploy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "875649023"
+    serviceWorkerVersion: "1325655113"
   }
 });
