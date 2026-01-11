351a4580031613134c07bfd4ca9d4f84816d383a
ethicnology <ethicnology@pm.me>
2026-01-11 18:29:02 -0500

    feat: Add window size management for desktop platforms

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,27 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:subterfuge/features/home/page.dart';
 import 'package:subterfuge/shared/theme.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() => runApp(const MyApp());
+const Size _minimalSize = Size(400, 500);
+const Size _defaultSize = Size(900, 600);
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    await windowManager.ensureInitialized();
+
+    await windowManager.setMinimumSize(_minimalSize);
+    await windowManager.setSize(_defaultSize);
+    await windowManager.center();
+    await windowManager.show();
+  }
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import screen_retriever_macos
 import url_launcher_macos
+import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
